238bd9f15aeb84a7372f31a51f970fd3c28df6a1
Siddhant Kala <siddhant03kala@gmail.com>
2024-01-17 23:40:22 +0530

    Contact Us Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Hero from "./components/Hero/Hero";
 import './App.css'
 import Companies from "./components/Companies/Companies";
 import YoutubeVideos from "./components/YoutubeVideos/YoutubeVideos";
+import Value from "./components/Value/Value";
+import Contact from "./components/Contact/Contact";
 function App() {
   return (
     <div className="App">
@@ -13,6 +15,8 @@ function App() {
       </div>
       <Companies/>
       <YoutubeVideos/>
+      <Value/>
+      <Contact/>
     </div>
   );
 }
